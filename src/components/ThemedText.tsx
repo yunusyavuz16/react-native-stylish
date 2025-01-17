c4b6191f438bbe4118433ad3c0e7ed5b3174c7e2
Yunus <yyavuz15@ford.com.tr>
2025-01-18 01:04:14 +0300

    update ThemedText component to use textColorStyle for improved styling consistency

--- src/components/ThemedText.tsx
+++ src/components/ThemedText.tsx
@@ -4,7 +4,7 @@ import useStyles from '../hooks/useStyles';
 export type ThemedViewProps = TextProps;
 
 export function ThemedText({ style, ...otherProps }: ThemedViewProps) {
-  const textColor = useStyles().defaultTextColor;
+  const textColor = useStyles().textColorStyle;
 
   return <Text style={[textColor, style]} {...otherProps} />;
 }
